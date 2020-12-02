bf3fd3a4501a1bcda687f6653346dff54d233bca
joseph <atemkuh@icloud.com>
2020-12-02 10:23:26 -0500

    scrape.py updated

--- scrape_hw.py
+++ scrape_hw.py
@@ -1,1 +1,26 @@
 #web scraping file
+from splinter import Browser
+from bs4 import BeautifulSoup
+
+
+def init_browser():
+    # actual path to the chromedriver
+    executable_path = {'executable_path': ChromeDriverManager().install()}
+    browser = Browser('chrome', **executable_path, headless=False)
+
+
+def scrape():
+    browser = init_browser()
+    country_list = {}
+
+    url = "https://statpedia.com/stat/Average_Yearly_Temperature_by_Country/HJd7OcGK"
+    browser.visit(url)
+
+    html = browser.html
+    soup = BeautifulSoup(html, "html.parser")
+
+    country_list["headline"] = soup.find("td", class_="htDimmed current area").get_text()
+    country_list["Average_Yearly_Temperature_by_Country "] = soup.find("td", class_="htDimmed").get_text()
+
+
+    return listings
